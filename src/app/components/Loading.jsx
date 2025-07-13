fabaa07933c951f3f730d9f2f12f981e19dd435b
Xiaozhe_Nice <3564660937@qq.com>
2025-07-13 12:47:31 +0800

    Update Loading.jsx

--- src/app/components/Loading.jsx
+++ src/app/components/Loading.jsx
@@ -6,22 +6,32 @@ const Loading = () => {
   return (
     <div className="loading-wrapper">
       <div className="loading-circle">
-        <img style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} src={'./logo.png'} alt="UniMC Logo" width={80} height={80} />
+        <img 
+          src={'./logo.png'} 
+          alt="UniMC Logo" 
+          width={80} 
+          height={80} 
+          className="logo-image"
+        />
       </div>
 
       <style jsx>{`
         .loading-wrapper {
-          display: flex;
-          align-items: center;
-          justify-content: center;
+          width: 100vw;
           height: 100vh;
           background: linear-gradient(315deg, #b8d0ffff 20%, #fff 80%,#fff 80%);
         }
 
         .loading-circle {
-          position: relative;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
           width: 120px;
           height: 120px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
         }
 
         .loading-circle::before {
@@ -39,15 +49,31 @@ const Loading = () => {
 
         @keyframes spin {
           0% {
-            transform: rotate(0);
+            transform: rotate(0deg);
           }
           100% {
             transform: rotate(360deg);
           }
         }
 
-        :global(img) {
+        :global(.logo-image) {
           border-radius: 50%;
+          z-index: 1;
+        }
+
+        :global(*) {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+
+        :global(html, body) {
+          --primary-color: #274B93;
+          width: 100%;
+          height: 100%;
+          font-family: 'Open Sans', sans-serif;
+          font-size: 16px;
+          overflow: hidden;
         }
       `}</style>
     </div>
